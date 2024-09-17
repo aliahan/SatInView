3c2543e721131e017f196b760819cc5c7ff7b3eb
clarkzjw <i@jinwei.me>
2024-09-16 23:13:41 -0700

    update README

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # Trajectory-based Serving Satellite Identification with User Terminal's Field-of-View
 
-This repository contains the implementation and artifacts for the paper ***Trajectory-based Serving Satellite Identification with User Terminal's Field-of-View*** submitted to LEO-NET'24.
+This repository contains the implementation and artifacts for the paper ***Trajectory-based Serving Satellite Identification with User Terminal's Field-of-View*** accepted in LEO-NET'24.
 
 Table of Contents
 =================
 
 * [Prerequisites](#prerequisites)
+* [Get new results](#get-new-results)
 * [Re-generate paper results](#re-generate-paper-results)
-* [Collect new data](#collect-new-data)
 
 ## Prerequisites
 
 Clone the repository.
 
 ```bash
-git clone --recurse-submodules https://github.com/<REDACTED>/SatInView.git
+git clone --recurse-submodules https://github.com/aliahan/SatInView.git
 cd SatInView
 ```
 
@@ -27,17 +27,43 @@ pip install -r requirements.txt
 pip install -r starlink-grpc-tools/requirements.txt
 ```
 
+Install `grpcurl` from https://github.com/fullstorydev/grpcurl.
+
+### Set variables
+
+1. Edit `dish_id` in `snr.py` to identify results from different dishes
+2. The total execuation time of the script is defined by the number of `exp_rounds` in `snr.py` times 15 seconds.
+
+## Get new results
+
+```bash
+bash status.sh
+python3 snr.py
+```
+
+You can run `tail -f white_pixel_coordinates_xor-{dish_id}.csv`, if there is anything like
+
+```
+2024-08-31 16:43:30,31,54
+2024-08-31 16:43:31,31,54
+2024-08-31 16:43:32,31,55
+2024-08-31 16:43:33,31,55
+2024-08-31 16:43:34,31,56
+2024-08-31 16:43:35,31,56
+2024-08-31 16:43:36,31,57
+2024-08-31 16:43:37,31,57
+2024-08-31 16:43:38,31,58
+```
+
+then it's running normally.
+
+Wait until the script finishes, you will get `gp.php`, `white_pixel_coordinates_xor-{dish_id}.csv`, and `get_status-{time}.txt` in the current directory.
+
 ## Re-generate paper results
-Run `snr.py` to collect new traces for Starlink satellite obstruction data. You will have the ` white_pixel_coordinates_xor.csv ` file.
 
-Run `findmatch.ipynb` notebook to first create the `processed_observed_data.csv` containing the topocentric coordinates of the observed satellites and then to find 
-the matching satellites and have the `matched_satellite_data.csv` dataset. 
+Run `findmatch.ipynb` notebook to first create the `processed_observed_data.csv` containing the topocentric coordinates of the observed satellites and then to find
+the matching satellites and have the `matched_satellite_data.csv` dataset.
 
-Ensure you set your location, dish orientation, and the data collection time duration in the cade accordingly. 
+Ensure you set your location, dish orientation, and the data collection time duration in the cade accordingly.
 
 To re-generate the figures in the paper you can run the  `figure.ipynb` notebook.
-...
-
-## Collect new data
-
-Run `python3 ./snr.py` to collect new traces for Starlink satellite obstruction data.
--- snr.py
+++ snr.py
@@ -5,7 +5,6 @@ import csv
 import sys
 import time
 import numpy as np
-import matplotlib.pyplot as plt
 
 from scipy.ndimage import label
 from datetime import datetime, timedelta
@@ -13,6 +12,9 @@ from skyfield.api import load, wgs84, utc
 
 sys.path.append(os.path.abspath('./starlink-grpc-tools'))
 import starlink_grpc
+
+dish_id="test"
+exp_round=5000
 
 
 def capture_snr_data(duration_seconds, interval_seconds, context):
@@ -33,45 +35,12 @@ def capture_snr_data(duration_seconds, interval_seconds, context):
     return snapshots
 
 
-def save_white_pixel_coordinates(snapshots, start_time):
-    white_pixel_coords = set()  # Use a set to store unique coordinates
-    for snr_data in snapshots:
-        coords = np.argwhere(snr_data == 1)
-        for coord in coords:
-            white_pixel_coords.add(tuple(coord))  # Add each coordinate as a tuple to the set
-
-    with open('white_pixel_coordinates.txt', 'a') as f:
-        f.write(f"{start_time}:\n")
-        for coord in sorted(white_pixel_coords):  # Sort coordinates for consistency
-            f.write(f"{coord}\n")
-        f.write("\n")
-
-
-def plot_snr_traces(all_snapshots, start_times, end_times, r):
-    plt.figure(figsize=(30, 40))  # Increase the figure size for better resolution
-    for i, (snapshots, start_time, end_time) in enumerate(zip(all_snapshots, start_times, end_times)):
-        base_image = np.zeros_like(snapshots[0])
-        for snr_data in snapshots:
-            base_image = np.maximum(base_image, snr_data)
-
-        plt.subplot(r//2+1, 2, i + 1)  # Arrange plots in a grid
-        plt.imshow(base_image, cmap='gray', origin='lower')
-        plt.title(f'Interval {i+1}\n{start_time} to {end_time} UTC')
-        plt.axis('off')
-
-    plt.suptitle('Starlink Obstruction Map Traces (15 intervals of 14 seconds each)')
-    plt.tight_layout(rect=[0, 0.03, 1, 0.95])  # Adjust layout for the main title
-    plt.savefig('trajectory2.png')
-    plt.show()
-
-
 def save_white_pixel_coordinates_xor(snapshots, start_time):
-    # Parse start_time back to a datetime object
     start_time_dt = datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
     previous_snr_data = np.zeros_like(snapshots[0][1])
     white_pixel_coords = []
 
-    with open('white_pixel_coordinates_xor.csv', 'a', newline='') as csvfile:
+    with open('white_pixel_coordinates_xor-{}.csv'.format(dish_id), 'a', newline='') as csvfile:
         writer = csv.writer(csvfile)
         i = 0
         hold_coord = None  # Initialize as None
@@ -88,32 +57,12 @@ def save_white_pixel_coordinates_xor(snapshots, start_time):
             else:
                 continue  # If both coords is empty and hold_coord is None, skip this iteration
 
-            # Add the timedelta to start_time_dt
             white_pixel_coords.append((start_time_dt + timedelta(seconds=i), tuple(coord)))
             previous_snr_data = snr_data
             i += 1
 
         for coord in white_pixel_coords:
             writer.writerow([coord[0].strftime("%Y-%m-%d %H:%M:%S"), coord[1][0], coord[1][1]])
-
-
-def filter_unconnected_white_pixels(snapshots):
-    final_snapshot = snapshots[-1]
-    labeled_array, num_features = label(final_snapshot)
-    connected_pixels = set()
-
-    for i in range(1, num_features + 1):
-        coords = np.argwhere(labeled_array == i)
-        if len(coords) > 1:  # More than one pixel in the component
-            for coord in coords:
-                connected_pixels.add(tuple(coord))
-
-    filtered_snapshot = np.zeros_like(final_snapshot)
-    for coord in connected_pixels:
-        filtered_snapshot[coord] = 1
-
-    snapshots[-1] = filtered_snapshot
-    return snapshots
 
 
 def wait_until_target_time():
@@ -135,16 +84,14 @@ def load_satellites():
 def main():
     load_satellites()
 
-    # Initialize the gRPC context
     context = starlink_grpc.ChannelContext(target="192.168.100.1:9200")
 
     all_snapshots = []
     start_times = []
     end_times = []
-    r = 500
 
-    for i in range(r):
-        print("Round {}/{}".format(i, r))
+    for i in range(exp_round):
+        print("Round {}/{}".format(i, exp_round))
         wait_until_target_time()
         starlink_grpc.reset_obstruction_map(context)
         duration_seconds = 14
@@ -154,23 +101,13 @@ def main():
         start_time= S.strftime("%Y-%m-%d %H:%M:%S")
         snapshots = capture_snr_data(duration_seconds, interval_seconds, context)
 
-        # Filter unconnected white pixels in the last snapshot
-        # snapshots = filter_unconnected_white_pixels(snapshots)
-
         end_time = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
 
         all_snapshots.append(snapshots)
         start_times.append(start_time)
         end_times.append(end_time)
 
-        # Save white pixel coordinates to a file
-        # save_white_pixel_coordinates(snapshots, start_time)
-
-        # Save white pixel coordinates with XOR to a CSV file
         save_white_pixel_coordinates_xor(snapshots, start_time)
-
-    # Plot all the traces
-    # plot_snr_traces(all_snapshots, start_times, end_times, r)
 
 
 if __name__ == "__main__":
